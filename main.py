ff9a9ccac7f6e92faf489c8e54c2124a595d2a4c
Саша Чедукаев <cedukaevsasa@gmail.com>
2025-04-04 20:25:13 +0700

    5th class

--- main.py
+++ main.py
@@ -4,41 +4,126 @@ application = Flask(__name__)
 
 
 
-@application.route("/promotion_image")
+@application.route("/astronaut_selection")
 def simple():
     return f'''
 <!doctype html>
-<html lang="en">
+<html lang="en">    
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="static/css/style.css">
     <link rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
     crossorigin="anonymous">
-    <link rel="stylesheet" href="static/css/style.css">
-    <title>Привет, Яндекс!</title>
+    <title>Отбор Астронавтов</title>
   </head>
   <body>
-    <img src="{url_for('static', filename='img/mars.png')}">
-    <h1>Привет, Яндекс!</h1>
-    <div class="alert alert-secondary" role="alert">
-        <br><b>Человечество вырастает из детства.</b>
-    </div>
-    <div class="alert alert-success" role="alert">
-        <br><b>Человечеству мала одна планета.</b>
-    </div>
-    <div class="alert alert-secondary" role="alert">
-        <br><b>Мы сделаем обитаемыми безжизненные пока планеты.</b>
-    </div>
-    <div class="alert alert-warning" role="alert">
-        <br><b>И начнем с Марса!</b>
-    </div>
-    <div class="alert alert-danger" role="alert">
-        <br><b>Присоединяйся!</b>
-    </div>
+    <main>
+      <h1>Анкета претендента</h1>
+      <h3>на участие в миссии</h3>
+      <section class="form">
+        <div class="form-floating">
+          <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Введите фамилию</label>
+        </div>
+        <div class="form-floating mb-5">
+          <input type="text" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Введите имя</label>
+        </div>
+        <div class="form-floating">
+          <input type="text" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Введите адрес почты</label>
+        </div>
+        <p>Какое у вас образование</p>
+        <select class="form-select form-select-lg mb-3" aria-label="Large select example">
+          <option value="1">Начальное</option>
+          <option value="2">Среднее</option>
+          <option value="3">Высшее</option>
+        <p>Какие к вас есть профессии?</p>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+          <label class="form-check-label" for="checkDefault">
+            Инженер-исследователь
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+          <label class="form-check-label" for="checkChecked">
+            Инженер-строитель
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+          <label class="form-check-label" for="checkChecked">
+            Пилот
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+          <label class="form-check-label" for="checkChecked">
+            Метеоролог
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+          <label class="form-check-label" for="checkChecked">
+            Инженер ро жизнеобеспеченью
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+          <label class="form-check-label" for="checkChecked">
+            Инженер по радиационной защите
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+          <label class="form-check-label" for="checkChecked">
+            Врач
+          </label>
+        </div>
+        <div class="form-check mb-4">
+          <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+          <label class="form-check-label" for="checkChecked">
+            Магистр по поеданию печенек
+          </label>
+        </div>
+        <p>Укажите пол</p>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="radioDefault" id="radioDefault1" checked>
+          <label class="form-check-label" for="radioDefault1">
+            Мужской
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="radioDefault" id="radioDefault2">
+          <label class="form-check-label" for="radioDefault2">
+            Женский
+          </label>
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Почему вы решили принять участие в миссии?</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <div class="mb-3">
+          <input type="file" class="form-control" aria-label="file example" required>
+        <div class="invalid-feedback">Выберите файл</div>
+        </div>
+        <div class="form-check mb-4">
+          <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+          <label class="form-check-label" for="checkChecked">
+            Готовы остаться на марсе?
+          </label>
+        </div>
+        <div class="mb-3">
+          <button class="btn btn-primary" type="submit">Отправить</button>
+        </div>
+      </section>
+    </main>
   </body>
-</html>
+</html> 
 '''
 
 
